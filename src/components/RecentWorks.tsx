67832110a6ad19d91e3f64bdbbcafef31ad54efa
skip5this <scottbell@Scotts-MacBook-Pro-3.local>
2025-06-09 21:50:55 -0700

    Reduce Scott Bell font weight to medium

--- src/components/RecentWorks.tsx
+++ src/components/RecentWorks.tsx
@@ -4,7 +4,7 @@ export function RecentWorks() {
   return (
     <div className="w-full bg-hero">
       <div className="max-w-7xl mx-auto px-4 md:px-8 py-12 md:py-16 min-h-[480px] md:min-h-[600px] lg:min-h-[760px] flex flex-col justify-center">
-        <div className="text-2xl md:text-3xl lg:text-4xl font-diatype-mono mb-4 font-bold">Scott Bell</div>
+        <div className="text-2xl md:text-3xl lg:text-4xl font-diatype-mono mb-4 font-medium">Scott Bell</div>
         <h2 className="text-5xl md:text-6xl lg:text-7xl font-diatype">Recent works</h2>
       </div>
     </div>
